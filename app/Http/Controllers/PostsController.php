a86362ee1cb07ee588da4e84ae7fcbb4e4d2a635
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2020-05-03 04:50:18 +0000

    2st

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -41,10 +41,9 @@ class PostsController extends Controller
         $post = new Post;
         $post->caption = $request->caption;
         $post->user_id = Auth::user()->id;
-
+        $post->image = base64_encode(file_get_contents($request->photo));
         $post->save();
         
-        $request->photo->storeAs('public/post_images', $post->id . '.jpg');
         return redirect('/');
     }
     
