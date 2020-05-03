c77b8843e45c5689477863316fd54e426ba7f514
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2020-05-03 04:30:35 +0000

    1st

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+Route::get('/', 'PostsController@index');
+Route::get('/home', 'PostsController@index');
+Route::get('/users/edit', 'UsersController@edit');
+Route::post('/users/update', 'UsersController@update');
+Route::get('/users/{user_id}', 'UsersController@show');
+Route::get('/posts/new', 'PostsController@new')->name('new');
+Route::post('/posts', 'PostsController@store');
+Route::get('/postsdelete/{post_id}', 'PostsController@destroy');
+Route::get('/posts/{post_id}/likes', 'LikesController@store');
+Route::get('/likes/{like_id}', 'LikesController@destroy');
+Route::post('posts/{comment_id}/comments', 'CommentsController@store');
+Route::get('comments/{comment_id}', 'CommentsController@destroy');
